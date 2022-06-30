ca4eb6c1634e186e68f6fb34a331dd0b508a769d
JEONGMUN LEE <lot9616@gmail.com>
2022-06-30 16:58:34 +0900

    임시저장

--- server/views.py
+++ server/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
+from .models import Server
 # Create your views here.
+
+
+class ServerListView(ListAPIView):
+    queryset = Server.objects.all()
+    serializer_class = ServerListSerializer
