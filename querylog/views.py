0e1ee4f036130fa2c21ac40b14584819f7a2ae50
Hyun Jun Lee <mikey1000@naver.com>
2022-07-01 15:54:50 +0900

    <Add> : Adjust CustomPagination Pagination count for total pages

--- querylog/views.py
+++ querylog/views.py
@@ -2,14 +2,28 @@ from django.shortcuts import render
 from .serializers import *
 from .models import Querylog
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from collections import OrderedDict
 
 
+class QuerylogListPagination(PageNumberPagination):
+    page_size = 17
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.num_pages),
+            ('results', data)
+        ]))
+        
 # Create your views here.
 
 
 class QuerylogListView(ListAPIView):
     queryset = Querylog.objects.all()
     serializer_class = QuerylogListSerializer
+    pagination_class = QuerylogListPagination
+
 
 
 class QuerylogSRregisterView(UpdateAPIView):
@@ -21,3 +35,6 @@ class QuerylogSRregisterView(UpdateAPIView):
 class QuerylogCreateView(CreateAPIView):
     queryset = Querylog.objects.all()
     serializer_class = QuerylogCreateSerializer
+
+class QuerylogExtractView(ListAPIView):
+    queryset = Querylog.objects.all()
