550e5e2cdfd9b84cc52fe3555564b1f0d4025b43
JEONGMUN LEE <lot9616@gmail.com>
2022-07-01 16:38:48 +0900

    <Fix> url 수정 url 수정

--- server/urls.py
+++ server/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from server.views import ServerListView
 
 urlpatterns = [
-    path('server/', ServerListView.as_view(), name = 'server_list')
+    path('server', ServerListView.as_view(), name = 'server_list')
 ]
