80ee2df6e8fcd0b835de1a5d99e4a0fc88ce06cf
JEONGMUN LEE <lot9616@gmail.com>
2022-07-01 10:07:35 +0900

    <Add> Port 추가 Server 모델에 Port 추가

--- server/models.py
+++ server/models.py
@@ -5,3 +5,4 @@ class Server(models.Model):
     name = models.CharField(max_length=20)
     ip_address = models.CharField(max_length=15)
     os_ver = models.CharField(max_length=10)
+    port = models.CharField(max_length=4)
