1ba344a921f4def3b5ced03130c858d1ad6bdecf
JEONGMUN LEE <lot9616@gmail.com>
2022-07-04 14:27:14 +0900

    <Add> Dashboard API 추가 sr 승인 비율과 server list를 포함한 GET API 만들어줌

--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from server.views import ServerListView
+from server.views import DashBoardView
 
 urlpatterns = [
-    path('server', ServerListView.as_view(), name = 'server_list')
+    # path('server', ServerListView.as_view(), name = 'server_list'),
+    path('dashboard', DashBoardView.as_view(), name='dashboard'),
 ]
--- server/views.py
+++ server/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from querylog.models import Querylog
 from .models import Server
 from .serializers import ServerListSerializer
-# Create your views here.
 
+# class ServerListView(ListAPIView):
+#     queryset = Server.objects.all()
+#     serializer_class = ServerListSerializer
 
-class ServerListView(ListAPIView):
+class DashBoardView(ListAPIView):
     queryset = Server.objects.all()
     serializer_class = ServerListSerializer
+
+    def list(self, request, *args, **kwargs):
+        
+        queryset = self.get_queryset()
+        not_approved_sr = Querylog.objects.filter(sr_number__isnull=True).count()
+        approved_sr = Querylog.objects.exclude(sr_number__isnull=True).count()
+        sr_info = {"approved" : approved_sr, "not_approved" : not_approved_sr}
+        servers= self.get_serializer(queryset, many=True).data
+        serializer = {'sr_info':sr_info, 'servers': servers}
+
+        return Response(serializer)
