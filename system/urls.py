61af60f54d9b69e07737d8c477b35324cec4b962
plily93 <agnespark0413@gmail.com>
2022-07-04 09:24:58 +0900

    <Fix> add trailing_slash=False
    delete slash at the end of each URI

--- system/urls.py
+++ system/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.routers import SimpleRouter
 from system.views import SystemViewSet
 
 router = routers.DefaultRouter()
-#router.trailing_slash=""
-# router = routers.SimpleRouter(trailing_slash=False)      
+router = routers.SimpleRouter(trailing_slash=False)      # router.trailing_slash=""
 router.register('system',SystemViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
 ]
+
+
+
+# class OptionalSlashRouter(SimpleRouter):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.trailing_slash = '/?'
