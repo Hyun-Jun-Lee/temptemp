f36fea7752155a47505414d88d74714c85aa3247
JEONGMUN LEE <lot9616@gmail.com>
2022-07-05 09:15:02 +0900

    <Fix> Listfield에 child 지정

--- system/serializers.py
+++ system/serializers.py
@@ -4,7 +4,7 @@ from .models import System
 
 # System Serializer
 class SystemSerializer(serializers.ModelSerializer):
-    table = serializers.ListField(source='tables', write_only=True, default=0)
+    table = serializers.ListField(source='tables', write_only=True, child=serializers.IntegerField(min_value=1))
     class Meta:
         model = System
         fields = ('id', 'name', 'description', 'tables', 'table')
